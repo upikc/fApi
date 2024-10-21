50e4ec3d774c75901ec3b0f700fdc024292b2bad
upik <uupikc@gmail.com>
2024-10-22 03:29:52 +0500

    test

--- TestApi/obj/Debug/net8.0/TestApi.AssemblyInfo.cs
+++ TestApi/obj/Debug/net8.0/TestApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b85c507cbe0be760a6b7939b72ea48c8fdf1c31")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
